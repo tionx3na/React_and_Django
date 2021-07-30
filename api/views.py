f8d2525eaf316dfb7acc1dcda9dc007fd4d4cada
tionx3na <1runx3na2@gmail.com>
2021-07-30 12:03:55 +0530

    second commit

---
+++ api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('It is working'),
+    # Since we are creating an api we do not need to return an html template, instead we return a response
