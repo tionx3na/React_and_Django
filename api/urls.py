bb23114f66530a10a31458c7b4b858813fa82591
xena <1runx3na2@gmail.com>
2021-08-02 01:11:01 +0530

    Registration System

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 # from . import views
-from .views import ArticleViewSet
+from .views import ArticleViewSet, UserViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('articles', ArticleViewSet, basename='articles')
+router.register('users', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     # path('articles/', views.ArticleList.as_view()),
     # path('articles/<int:id>', views.ArticleDetails.as_view()),
     # path('articles', views.article_list),
