803a3c31b8cdd1e18ba9dd949d55f4d6c848c422
xena <1runx3na2@gmail.com>
2021-08-02 16:18:40 +0530

    Class Components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './components/hello';
 import Temp from './components/temp';
+import MyClass from './components/classcomponent';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>Hello World! </h1>
       <Hello/>
       <Temp/>
+      <MyClass/>
     </div>
   );
 }
---
+++ src/components/classcomponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MyClass extends Component {
+    render() {
+        return (
+            <h1>Now i am inside Class component</h1>
+            )
+    }
+}
+
+export default MyClass;
