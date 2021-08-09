c5b0cd3c3fa05f958e679f0bda00bd52f0780e6f
xena <1runx3na2@gmail.com>
2021-08-10 01:04:39 +0530

    corrections

--- reactdjango/settings.py
+++ reactdjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g6*7h#s6)w@7cnf$$x=l_2qtzz3*7$i8=6+#hbrz%ed+)dw5ot'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
